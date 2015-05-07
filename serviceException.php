c125df59106eb6a0d04922ed3828314db0d76354
stenvala <antti@stenvall.fi>
2015-05-07 15:29:44 +0300

    Added serviceException which allows one to return exception parameters as JSON

---
+++ serviceException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace slimClass;
+
+class serviceException extends \Exception {
+
+  private $params;
+  
+  public function __construct($message, $code = 0, $params = array(), \Exception $previous = null) {
+    parent::__construct($message, $code, $previous);
+    $this->params = $params;
+  }
+  
+  public function params(){
+    return $params;
+  }
+
+}
